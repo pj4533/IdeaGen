b5052eba6f58e674c771500541c244ade65cad3e
PJ Gray <pj4533@gmail.com>
2025-03-17 17:29:03 -0400

    Initial Commit

---
+++ IdeaGen/IdeaGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaGenApp.swift
+//  IdeaGen
+//
+//  Created by PJ Gray on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct IdeaGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IdeaGenTests/IdeaGenTests.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaGenTests.swift
+//  IdeaGenTests
+//
+//  Created by PJ Gray on 3/17/25.
+//
+
+import Testing
+@testable import IdeaGen
+
+struct IdeaGenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
